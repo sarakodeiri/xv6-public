ca434c1837839db5c1982c411291505f1b09a279
Sara <skaordaeiri@gmail.com>
2020-01-03 21:35:50 +0330

    Q1 done

--- onetest.c
+++ onetest.c
@@ -8,17 +8,17 @@ int main (int argc,char *argv[])
   int w, r, pid;
   pid = fork();
   if (pid == 0){
-    // sleep(500);
+    sleep(50);
     for (int i=0; i<1000; i++){
-        printf(1, "1 sacsd\n");
+        printf(1, "%d\n", i);
     }
       // printf(1, "1 ");
   }
 
 
   else if (pid > 0){
-    waitx(&w, &r);
-    printf(1, "Wtime: %d Rtime: %d\n", w, r);
+    int res = waitx(&w, &r);
+    printf(1, "Wtime: %d Rtime: %d Status:%d\n", w, r, res);
   }
   
   exit();
